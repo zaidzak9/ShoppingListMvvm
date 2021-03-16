5563766390eb0c3d8d701beb4fa41c7bb85bae74
ZaidZakir <zaidzakir@hlbb.hongleong.com.my>
2021-03-16 13:22:37 +0800

    Initial commit

---
+++ app/src/main/java/com/zaidzakir/shoppinglistmvvm/ShoppingActivity.kt
@@ -0,0 +1,12 @@
+package com.zaidzakir.shoppinglistmvvm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShoppingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
